d2896b5045622c7275c3c701bc5bf0e7fbe7d859
Rui Ventura <rventura.pt@outlook.com>
2019-10-20 15:31:57 +0100

    Expand gcc versions by default

--- build_tarballs.jl
+++ build_tarballs.jl
@@ -61,7 +61,7 @@ cmake --build . --config Release --target install
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-const platforms = supported_platforms()
+const platforms = expand_gcc_versions(supported_platforms())
 
 # The products that we will ensure are always built
 products(prefix) = [
